c99e8ffa44161db6c8349d0e029088ffb4e6c662
Camzcamz <30909107+Camzcamz@users.noreply.github.com>
2019-01-06 05:42:43 -0500

    Add files via upload

---
+++ Experimental_Lists.py
@@ -0,0 +1,22 @@
+######## Generate Color Stimuli Data File #######
+import csv, os, glob
+
+with open('Task_Stimuli_Color.csv', 'w') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(["Stimulus", "Type","Filenumber"])
+    for filename in glob.glob('Stimuli_Colors/*.jpg'): #Glob does not take regular expressions but takes * which corresponds to 'anything' afterwards
+        filename = os.path.basename(filename) # remove folder
+        basename, ext = os.path.splitext(filename) # remove extension
+        stimuli, type, filenumber = basename.split() # split into 3 columns
+        filename = stimuli, type, filenumber
+        writer.writerow(filename)
+
+with open('Task_Stimuli_Shape.csv', 'w') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(["Stimulus", "Type","Filenumber"])
+    for filename in glob.glob('Stimuli_Shapes/*.jpg'): #Glob does not take regular expressions but takes * which corresponds to 'anything' afterwards
+        filename = os.path.basename(filename) # remove folder
+        basename, ext = os.path.splitext(filename) # remove extension
+        stimuli, type, filenumber = basename.split() # split into 3 columns
+        filename = stimuli, type, filenumber
+        writer.writerow(filename)
