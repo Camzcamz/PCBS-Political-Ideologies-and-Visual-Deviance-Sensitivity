6345c182775805bd360c4bc75e1427c851385122
Camzcamz <30909107+Camzcamz@users.noreply.github.com>
2019-01-11 14:50:54 +0100

    Update Experimental_Lists.py

--- Experimental_Lists.py
+++ Experimental_Lists.py
@@ -1,7 +1,7 @@
 ######## Generate Stimuli Data Files #######
 import csv, os, glob
 
-with open('Task_Stimuli_Color.csv', 'w') as csvfile:
+with open('Task_Stimuli_Colors.csv', 'w') as csvfile:
     writer = csv.writer(csvfile)
     writer.writerow(["Stimulus", "Type","Filenumber"])
     for filename in glob.glob('Stimuli_Colors/*.jpg'): #Glob does not take regular expressions but takes * which corresponds to 'anything' afterwards
@@ -11,7 +11,7 @@ with open('Task_Stimuli_Color.csv', 'w') as csvfile:
         filename = stimuli, type, filenumber
         writer.writerow(filename)
 
-with open('Task_Stimuli_Shape.csv', 'w') as csvfile:
+with open('Task_Stimuli_Shapes.csv', 'w') as csvfile:
     writer = csv.writer(csvfile)
     writer.writerow(["Stimulus", "Type","Filenumber"])
     for filename in glob.glob('Stimuli_Shapes/*.jpg'): #Glob does not take regular expressions but takes * which corresponds to 'anything' afterwards
