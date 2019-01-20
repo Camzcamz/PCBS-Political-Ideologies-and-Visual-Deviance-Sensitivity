f25a55899e4454331838ac4ecac159338983f6e1
Camzcamz <30909107+Camzcamz@users.noreply.github.com>
2019-01-20 10:48:08 +0100

    Update Colors.py

--- Colors.py
+++ Colors.py
@@ -17,11 +17,10 @@ SQUARE = pygame.Rect(left, top, width, height) #takes four parameters even if yo
 # start
 pygame.init()
 screen = pygame.display.set_mode((W, H), pygame.DOUBLEBUF)
-# no need her to define BG color or screen.fill( )
 
 for c,v in Colors.items():
     print (c,v)
-    pygame.draw.rect(screen, v, SQUARE)# v = color value of specific key
-    pygame.image.save(screen, "Stimuli_Colors/%s.jpg" %c) # adds c instead of %s, a dictionary key for a specific v, value
+    pygame.draw.rect(screen, v, SQUARE)# value (v) of key (c) 
+    pygame.image.save(screen, "Stimuli_Colors/%s.jpg" %c) # adds c instead of %s
 
 pygame.display.flip() # allows for update of contents of the entire display
