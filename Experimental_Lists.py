d2d165bb7bd5f3b245eb8a23824a03c6127cd35d
Camzcamz <30909107+Camzcamz@users.noreply.github.com>
2019-01-20 12:26:52 +0100

    Update Experimental_Lists.py

--- Experimental_Lists.py
+++ Experimental_Lists.py
@@ -1,22 +1,16 @@
 ######## Generate Stimuli Data Files #######
 import csv, os, glob
 
-with open('Task_Stimuli_Colors.csv', 'w') as csvfile:
-    writer = csv.writer(csvfile)
-    writer.writerow(["Stimulus", "Type","Filenumber"])
-    for filename in glob.glob('Stimuli_Colors/*.jpg'): #Glob does not take regular expressions but takes * ( = anything afterwards)
-        filename = os.path.basename(filename) # remove folder name 
-        basename, ext = os.path.splitext(filename) # remove extension
-        stimuli, type, filenumber = basename.split() # split into 3 columns
-        filename = stimuli, type, filenumber
-        writer.writerow(filename)
+def create_stim_file (filename, stim_path):
+    with open(filename, 'w') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(["Stimulus", "Type","Filenumber"])
+        for filename in glob.glob(stim_path): #Glob does not take regular expressions but takes * which corresponds to 'anything' afterwards
+            filename = os.path.basename(filename) # remove folder
+            basename, ext = os.path.splitext(filename) # remove extension
+            stimuli, type, filenumber = basename.split() # split into 3 columns
+            filename = stimuli, type, filenumber
+            writer.writerow(filename)
 
-with open('Task_Stimuli_Shapes.csv', 'w') as csvfile:
-    writer = csv.writer(csvfile)
-    writer.writerow(["Stimulus", "Type","Filenumber"])
-    for filename in glob.glob('Stimuli_Shapes/*.jpg'): 
-        filename = os.path.basename(filename) 
-        basename, ext = os.path.splitext(filename)
-        stimuli, type, filenumber = basename.split() 
-        filename = stimuli, type, filenumber
-        writer.writerow(filename)
+create_stim_file ('Task_Stimuli_Colors.csv', 'Stimuli_Colors/*.jpg')
+create_stim_file ('Task_Stimuli_Shapes.csv', 'Stimuli_Shapes/*.jpg')
